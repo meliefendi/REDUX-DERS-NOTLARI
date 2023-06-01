800af8fc852a673a9a973643b0941bff5f7ffd9b
Melih Akkas <melh_akkas@hotmail.com>
2023-06-01 19:44:28 +0300

    Add files via upload

---
+++ REDUX NOTES/2.REDUX STORE OLUŞTURMA/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        redux
+      </header>
+    </div>
+  );
+}
+
+export default App;
