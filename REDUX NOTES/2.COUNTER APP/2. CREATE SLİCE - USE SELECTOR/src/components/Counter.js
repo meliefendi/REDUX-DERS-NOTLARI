18efa2d003bf584c67ebadcbf31e155a7468e0b8
Melih Akkas <melh_akkas@hotmail.com>
2023-06-03 21:17:37 +0300

    Update Counter.js

--- REDUX NOTES/2.COUNTER APP/2. CREATE SLİCE - USE SELECTOR/src/components/Counter.js
+++ REDUX NOTES/2.COUNTER APP/2. CREATE SLİCE - USE SELECTOR/src/components/Counter.js
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 function Counter(){
     //counValue aslında store sayfasındaki reducer içerisindeki counter: counterReducetur o da counterSlice sayfasındaki counterSlice fonksiyonunun bize sunduğu değerlerdir.
     // VE DURUMU EKRANA YAZDIRMAMIZI SAĞLAYAN ARAÇ USESELECTOR
+    // ayrıca burdaki state globaldeki state yani store sayfasındaki gelen state şu an orada sadece counter var atıyorum pelcil olsa state.pencil şeklinde ekleme yapılacaktı.
     const countValue = useSelector((state) => state.counter.value)
     return(
         <div>
