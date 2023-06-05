8bf60c26a252fc5862bd91d3e257575695cabc8e
Melih Akkas <melh_akkas@hotmail.com>
2023-06-05 17:46:26 +0300

    Add files via upload

---
+++ REDUX NOTES/3.TODOAPP/1.GENEL GÖRÜNÜM HAZIRLANMASI/src/components/form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Form() {
+    return (
+        <div>
+            <form>
+                <input className="new-todo" placeholder="What needs to be done?" autoFocus />
+            </form>
+        </div>
+    )
+}
+
+export default Form;
---
+++ REDUX NOTES/3.TODOAPP/1.GENEL GÖRÜNÜM HAZIRLANMASI/src/components/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+//form componenti
+import Form from "./form";
+
+function Header(){
+    return(
+        <header className="header">
+		<h1>todos</h1>
+		
+        <Form/>
+
+	</header>
+    )
+}
+
+export default Header;
