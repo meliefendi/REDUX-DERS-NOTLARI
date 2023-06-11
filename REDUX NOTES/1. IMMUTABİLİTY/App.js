2ddf06e58c71e2c87ad3d6bf57626368e9f1cac5
Melih Akkas <melh_akkas@hotmail.com>
2023-06-11 21:27:07 +0300

    bootstrapApp.js

--- REDUX NOTES/1. IMMUTABİLİTY/App.js
+++ REDUX NOTES/1. IMMUTABİLİTY/App.js
@@ -9,6 +9,11 @@ function App() {
         <div className="d-grid gap-2 col-6 mx-auto">
           <button className="btn btn-primary" type="button">Button</button>
           <button className="btn btn-primary" type="button">Button</button>
+    
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" 
+    crossorigin="anonymous">
+    
         </div>
       </header>
     </div>
