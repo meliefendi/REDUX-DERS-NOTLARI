25b83ab2d69b24f58f1d4c056aa8a9303553b3e0
Melih Akkas <melh_akkas@hotmail.com>
2023-06-22 21:21:42 +0300

    Update index.js

--- REDUX NOTES/4.BREAKİNG BAD APP/5. KARAKTER DETAY EKRANININ HAZIRLANMASI/note/src/pages/Detail/index.js
+++ REDUX NOTES/4.BREAKİNG BAD APP/5. KARAKTER DETAY EKRANININ HAZIRLANMASI/note/src/pages/Detail/index.js
@@ -18,7 +18,14 @@ function Detail() {
     const character_id = characters.map((character) => (
         character
     ))
-    const searh_id = character_id.find(id => id.char_id == char_id)
+    const searh_id = character_id.find(id => id.char_id == char_id);
+
+    //EN SON EKLENMİŞTİR!!!
+    // BURASI DAHA ÖNCESİNDE SAYFA YENİLEME ESNASINDA HATA VERİP KİTLENMEYE YOL AÇIYORDU. FAKAT BU İŞLEMLE BUNUN ÖNÜNE GEÇTİK. ŞİMDİ SAYFA YENİLENİNCE ARAYIP BULAMADIĞI ELEMAN 
+    // OLURSA HOME SAYFASINA YÖNLENDİRME SAĞLANDI.
+     if (!searh_id) {
+        return <Navigate to="/" />
+    };
 
     return (
         <div>
